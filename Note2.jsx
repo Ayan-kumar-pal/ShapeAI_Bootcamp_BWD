c10290af54a534b783a7d3665e72d5c819247ed9
Ayan-kumar-pal <76846386+Ayan-kumar-pal@users.noreply.github.com>
2021-06-28 12:03:48 +0530

    Add files via upload

---
+++ Note2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note2()
+{
+return (
+   <div className="note">
+     <h1>Python and Natural Language Processing - Free 7 Days Long Bootcamp</h1>
+     <p>📢Here we bring our another free live BOOTCAMP with a hands-on project at the end - Foundation of Python and Natural Programming Processing🖥</p>
+   </div>
+
+   );
+
+}
+
+export default Note2 ;
